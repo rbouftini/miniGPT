c86faa8b652a586602de8e4279a7c04a4d53c3b7
Rayane Bouftini <rbouftin@iwu.edu>
2024-09-27 23:06:49 +0100

    configure new tokenizer

--- data/prep.py
+++ data/prep.py
@@ -2,43 +2,23 @@ import os
 import numpy as np
 import re
 from datasets import load_dataset
-from tokenizers import Tokenizer
+from transformers import AutoTokenizer
 from tqdm import tqdm
 
 # Load the dataset
-dataset = load_dataset("alielfilali01/Darija-Stories-Dataset", split="train")
+dataset = load_dataset("M-A-D/Mixed-Arabic-Datasets-Repo", "Ara--Wikipedia", split="train")
 
 # Split the dataset into training and validation sets
 split_dataset = dataset.train_test_split(test_size=0.01, seed=2357, shuffle=False)
 split_dataset['val'] = split_dataset.pop('test')
 
 # Load the tokenizer from the JSON file
-tokenizer = Tokenizer.from_file("tokenizer.json")
-
-# Function to remove emojis from text
-def remove_emojis(text):
-    emoji_pattern = re.compile(
-        "[" 
-        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        "\U0001F300-\U0001F5FF"  # symbols & pictographs
-        "\U0001F600-\U0001F64F"  # emoticons
-        "\U0001F680-\U0001F6FF"  # transport & map symbols
-        "\U0001F700-\U0001F77F"  # alchemical symbols
-        "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-        "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-        "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-        "\U0001FA00-\U0001FA6F"  # Chess Symbols
-        "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-        "\U00002702-\U000027B0"  # Dingbats
-        "\U000024C2-\U0001F251"
-        "]+" 
-    )
-    return emoji_pattern.sub(r'', text)
+tokenizer = AutoTokenizer.from_pretrained("tokenizer")
 
 # Function to process each example
 def process(example):
-    example = remove_emojis(example["Text"])
-    tokens_ids = tokenizer.encode(example).ids
+    tokens_ids = tokenizer.encode(example)
+    tokens_ids.append(tokens_ids.eos_token_id)
     return {"token_ids": tokens_ids, "len": len(tokens_ids)}
 
 # Process the dataset
@@ -46,7 +26,7 @@ new_dataset = split_dataset.map(
     process,
     num_proc=8,
     desc="Tokenizing and cleaning the dataset",
-    remove_columns=["ChapterName", "ChapterLink", "Author", "Text", "Tags"]
+    remove_columns=["id", "url", "title", "text"]
 )
 
 # Save the token IDs using memory mapping
--- data/tokenizer_train.py
+++ data/tokenizer_train.py
@@ -1,57 +1,26 @@
+import os
 from datasets import load_dataset
-import re
-from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace
+from transformers import AutoTokenizer
 
-# Load the dataset
-dataset = load_dataset("alielfilali01/Darija-Stories-Dataset", split="train")
-text = ''.join([text for text in dataset["Text"]])
+# Load dataset 
+dataset = load_dataset("M-A-D/Mixed-Arabic-Datasets-Repo", "Ara--Wikipedia", split="train")
 
-# Remove emojis
-def remove_emojis(text):
-    emoji_pattern = re.compile(
-        "["
-        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        "\U0001F300-\U0001F5FF"  # symbols & pictographs
-        "\U0001F600-\U0001F64F"  # emoticons
-        "\U0001F680-\U0001F6FF"  # transport & map symbols
-        "\U0001F700-\U0001F77F"  # alchemical symbols
-        "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-        "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-        "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-        "\U0001FA00-\U0001FA6F"  # Chess Symbols
-        "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-        "\U00002702-\U000027B0"  # Dingbats
-        "\U000024C2-\U0001F251"
-        "]+"
-    )
-    return emoji_pattern.sub(r'', text)
+HF_token = "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"
+model_id = "meta-llama/Meta-Llama-3.1-70B-Instruct"
 
-clean_text = remove_emojis(text)
+# Import Llama Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_id, token= HF_token)
 
-# Initialize a tokenizer
-tokenizer = Tokenizer(BPE())
+# Generate text batches
+def get_batches(batch_size=1000):
+  for i in range(0, len(dataset), batch_size):
+    yield dataset[i:i+batch_size]["text"]
 
-# Initialize a trainer with special tokens
-trainer = BpeTrainer(
-    special_tokens=["<|endoftext|>"],
-    vocab_size=16384  
-)
+# Retrain Llama Tokenizer    
+tokenizer = tokenizer.train_new_from_iterator(
+    get_batches(),
+    vocab_size = 32208,
+    show_progress = True)
 
-# Configure pre-tokenization
-tokenizer.pre_tokenizer = Whitespace()
-
-# Prepare the text for training
-def batch_iterator(batch_size=1000):
-    for i in range(0, len(clean_text), batch_size):
-        yield clean_text[i:i+batch_size]
-
-# Train the tokenizer
-tokenizer.train_from_iterator(batch_iterator(), trainer=trainer, length=len(clean_text))
-
-# Save the tokenizer
-tokenizer.save("tokenizer.json")
-
-print("Tokenizer trained and saved succesfully")
+# Save the new tokenizer
+tokenizer.save_pretrained("tokenizer")
