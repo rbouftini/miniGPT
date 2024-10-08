0cfa1b1edbd1671a914d79f7a94fa2a74fe88526
Rayane Bouftini <rbouftin@iwu.edu>
2024-10-08 23:41:12 +0100

    switch for the new tokenizer

--- generate.py
+++ generate.py
@@ -1,21 +1,16 @@
 import torch
 import pickle
-from model import MiniGPTConfig, MiniGPT
+from transformers import AutoTokenizer
+from model import MiniGPT, MiniGPTConfig
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-with open("tokenizer.pkl", "rb") as f:
-    tokenizer= pickle.load(f)
-
-itos, stoi = tokenizer["itos"], tokenizer["stoi"]
-
-encode = lambda s: [stoi[c] for c in s]
-decode = lambda l: ''.join([itos[i] for i in l])
+tokenizer = AutoTokenizer.from_pretrained("tokenizer")
 
 config = MiniGPTConfig()
 model = MiniGPT(config).to(device)
 
-model_dict = torch.load("checkpoints/step_8000.pt", map_location=torch.device('cpu'))
+model_dict = torch.load("checkpoints/step_4000.pt", map_location=device)
 def remove_prefix(state_dict):
     return {(k[10:] if k.startswith("_orig_mod.") else k): v for k, v in state_dict.items()}
 
@@ -23,6 +18,5 @@ model_dict = remove_prefix(model_dict["model"])
 model.load_state_dict(model_dict)
 
 model.eval()
-tokens = encode("هي قصة")
-text = decode(model.generate(torch.tensor(tokens, dtype=torch.long ,device=device).unsqueeze(0), max_new_tokens=1000)[0].tolist())
-
+example = "مدينة سان فرانسيسكو تقع"
+text = tokenizer.decode(model.generate(torch.tensor(tokenizer.encode(example), dtype=torch.long ,device=device).unsqueeze(0), max_new_tokens=100)[0].tolist())
