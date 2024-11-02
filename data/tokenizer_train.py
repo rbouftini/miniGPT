45cc248fa6131390a6d07b79a126754dd1646f5e
Rayane Bouftini <rbouftin@iwu.edu>
2024-11-02 20:45:21 +0100

    Merge new datasets and train new tokenizer

--- data/tokenizer_train.py
+++ data/tokenizer_train.py
@@ -1,24 +1,41 @@
 import os
-from datasets import load_dataset
 from transformers import AutoTokenizer
+from datasets import load_dataset, concatenate_datasets
+from dotenv import load_dotenv
 
-# Load dataset 
-dataset = load_dataset("M-A-D/Mixed-Arabic-Datasets-Repo", "Ara--Wikipedia", split="train")
+# Load the different datasets into a single merged one
+datasets = [
+    load_dataset("M-A-D/Mixed-Arabic-Datasets-Repo", name, split="train")
+    .remove_columns([col for col in load_dataset("M-A-D/Mixed-Arabic-Datasets-Repo", name, split="train").column_names if col != column])
+    .rename_column(column, "Text")
+    .filter(lambda example: example["Text"] is not None)
+    for name, column in [
+        ("Ara--Ali-C137--Hindawi-Books-dataset", "ChapterText"),
+        ("Ara--Wikipedia", "text"),
+        ("Ara--J-Mourad--MNAD.v1", "Body"),
+        ("Arz--Wikipedia", "text"),
+        ("Ara--saudinewsnet", "content")
+    ]
+]
 
-HF_token = "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"
-model_id = "meta-llama/Meta-Llama-3.1-70B-Instruct"
+merged_dataset = concatenate_datasets(datasets)
+
+#Load HuggingFace API key
+load_dotenv()
+HF_token = os.getenv('HF_TOKEN')
 
 # Import Llama Tokenizer
+model_id = "meta-llama/Meta-Llama-3.1-70B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_id, token= HF_token)
 
 # Generate text batches
 def get_batches(batch_size=1000):
-  for i in range(0, len(dataset), batch_size):
-    yield dataset[i:i+batch_size]["text"]
+  for i in range(0, len(merged_dataset), batch_size):
+    yield merged_dataset[i:i+batch_size]["Text"]
 
-# Retrain Llama Tokenizer    
+# Retrain Llama Tokenizer
 tokenizer = tokenizer.train_new_from_iterator(
-    get_batches(),
+    text_iterator = get_batches(),
     vocab_size = 32208,
     show_progress = True)
 
